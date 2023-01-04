c9d1d61a79ba67e88be3412c616ab97549e9da72
PROD\wdegruijl <wouter.degruijl@alten.nl>
2023-01-04 11:24:24 +0100

    Redesign HTML

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -1,29 +1,71 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 
+interface mixForm {
+  magSuppServing: FormControl<string>;
+  potSuppServing: FormControl<string>;
+  saltSuppServing: FormControl<string>;
+
+  magInServing: FormControl<string>;
+  potInServing: FormControl<string>;
+  saltInServing: FormControl<string>;
+
+  magDesired: FormControl<string>;
+  potDesired: FormControl<string>;
+  saltDesired: FormControl<string>;
+
+  servings: FormControl<string>;
+}
+
 @Component({
   selector: 'app-calculator',
   templateUrl: './calculator.component.html',
   styleUrls: ['./calculator.component.css'],
 })
 export class CalculatorComponent implements OnInit {
-  calculatorForm = new FormGroup({
-    magSuppServing: new FormControl('2500', [
-      Validators.required,
-      Validators.min(0),
-    ]),
-    potSuppServing: new FormControl('500', [Validators.required]),
-    saltSuppServing: new FormControl('1000', [Validators.required]),
+  calculatorForm = new FormGroup<mixForm>({
+    magSuppServing: new FormControl('2500', {
+      nonNullable: true,
+      validators: [Validators.required],
+    }),
+    potSuppServing: new FormControl('500', {
+      nonNullable: true,
+      validators: [Validators.required],
+    }),
+    saltSuppServing: new FormControl('1000', {
+      nonNullable: true,
+      validators: [Validators.required],
+    }),
 
-    magInServing: new FormControl('250', [Validators.required]),
-    potInServing: new FormControl('250', [Validators.required]),
-    saltInServing: new FormControl('400', [Validators.required]),
+    magInServing: new FormControl('250', {
+      nonNullable: true,
+      validators: [Validators.required],
+    }),
+    potInServing: new FormControl('250', {
+      nonNullable: true,
+      validators: [Validators.required],
+    }),
+    saltInServing: new FormControl('400', {
+      nonNullable: true,
+      validators: [Validators.required],
+    }),
 
-    magDesired: new FormControl('50'),
-    potDesired: new FormControl('300'),
-    saltDesired: new FormControl('1000'),
-
-    servings: new FormControl('40'),
+    magDesired: new FormControl('50', {
+      nonNullable: true,
+      validators: [Validators.required],
+    }),
+    potDesired: new FormControl('300', {
+      nonNullable: true,
+      validators: [Validators.required],
+    }),
+    saltDesired: new FormControl('1000', {
+      nonNullable: true,
+      validators: [Validators.required],
+    }),
+    servings: new FormControl('40', {
+      nonNullable: true,
+      validators: [Validators.required],
+    }),
   });
 
   constructor() {}
